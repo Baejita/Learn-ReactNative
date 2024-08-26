56c11cec8eee92398310b3805cf895fec138255a
Baejita <homokoro@gmail.com>
2024-08-26 13:27:50 +0700

    searchAPI

---
+++ Search Restuarant/food/src/api/yelp.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://api.yelp.com/v3/businesses",
+  headers: {
+    Authorization: "Bearer ",
+  },
+});
--- Search Restuarant/food/src/screens/Search.js
+++ Search Restuarant/food/src/screens/Search.js
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
 import { Text, View } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 function SearchSceen() {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 50,
+        term,
+        location: "san jose",
+      },
+    });
+    setResults(response.data.businesses);
+  };
+
   return (
     <View>
-      <SearchBar
-        term={term}
-        onTermChage={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={onTermSubmit}
-      />
+      <SearchBar term={term} onTermChage={setTerm} onTermSubmit={searchApi} />
       <Text>Search Screen</Text>
+      <Text>We have found {results}</Text>
     </View>
   );
 }
