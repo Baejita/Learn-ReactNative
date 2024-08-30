32ad67078bb72861bd1eb82a3e2800431d7024fd
Baejita <homokoro@gmail.com>
2024-08-30 11:58:17 +0700

    jsonserver

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: " https://514f-159-192-143-78.ngrok-free.app/",
+});
--- src/context/BlogProvider.js
+++ src/context/BlogProvider.js
@@ -1,56 +1,57 @@
-import createDataContext from './createDataContext';
+import createDataContext from "./createDataContext";
+import jsonServer from "../api/jsonServer";
 
+const blogPostReducer = (state, action) => {
+  switch (action.type) {
+    case "delete_blockPost":
+      return state.filter((blogpost) => blogpost.id !== action.payload);
 
-const blogPostReducer = (state, action) =>{
-    switch (action.type) {
+    case "add_blockPost":
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 9999),
+          title: action.payload.title,
+          content: action.payload.content,
+        },
+      ];
 
-        case 'delete_blockPost':
-            return state.filter(blogpost => blogpost.id !== action.payload)
+    case "edit_blockPost":
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
 
-        case 'add_blockPost' :
-            return [...state, {id: Math.floor(Math.random()* 9999), 
-            title: action.payload.title,
-            content: action.payload.content
-        }]
-        
-        case 'edit_blockPost':
-            return state.map(blogPost => {
-                return blogPost.id === action.payload.id ? action.payload : blogPost;
-            }
-                )
+    default:
+      return state;
+  }
+};
 
-        default :
-        return state;
+const addBlogPost = (dispatch) => {
+  return (title, content, callback) => {
+    dispatch({ type: "add_blockPost", payload: { title, content } });
+    if (callback) {
+      callback();
     }
+  };
+};
 
-}
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: "delete_blockPost", payload: id });
+  };
+};
 
-const addBlogPost = dispatch => { 
-    return (title, content, callback)=> {
-        dispatch({type : 'add_blockPost', payload: { title, content}})
-        if(callback) {
-
-            callback();
-        }
+const editBlogPost = (dispatch) => {
+  return (id, title, content, callback) => {
+    dispatch({ type: "edit_blockPost", payload: { id, title, content } });
+    if (callback) {
+      callback();
     }
-}
+  };
+};
 
-const deleteBlogPost = dispatch => {
-    return (id)=> {
-        dispatch({type: 'delete_blockPost', payload: id})
-    }
-}
-
-const editBlogPost = dispatch => { 
-    return (id , title, content, callback)=> {
-        dispatch({type : 'edit_blockPost', payload: {id, title, content}})
-        if(callback) {
-            callback();
-        }
-    }
-}
-
-
-
-
-export const {Context, Provider} = createDataContext(blogPostReducer, {addBlogPost, deleteBlogPost, editBlogPost}, [{title : 'Test Title', content : 'Test Content', id: 1}])
+export const { Context, Provider } = createDataContext(
+  blogPostReducer,
+  { addBlogPost, deleteBlogPost, editBlogPost },
+  [{ title: "Test Title", content: "Test Content", id: 1 }]
+);
